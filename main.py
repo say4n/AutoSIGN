5fdbfe25f13e05db889c82760b74ce063bec6026
Sayan Goswami <goswami.sayan47@gmail.com>
2018-03-05 15:21:56 +0530

    Flask for midend

--- main.py
+++ main.py
@@ -1,4 +1,32 @@
-import this
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+@app.route("/verify", methods=["POST"])
+def verify():
+    """
+    accepts POST of json data
+
+    data = {
+        "link_to_uploaded_signature" : url
+        "uid_of_customer" : uid
+    }
+    """
+
+    if request.method == "POST":
+        url = request.form.get("link_to_uploaded_signature")
+        uid = request.form.get("uid_of_customer")
+        
+        if urlparse(url).scheme in ["http", "https"]:
+            
+        else:
+            abort(400)
+
 
 if __name__ == "__main__":
-    print(42)
+    app.run(debug=True)
