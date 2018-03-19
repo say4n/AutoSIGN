9bf6faa15903759622b258b6c196b228dce27eff
Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>
2018-03-19 19:53:44 +0530

    Adding Signature Validation Functions and Models

--- main.py
+++ main.py
@@ -2,16 +2,62 @@ import os
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
 
+# Process_Folder
+from scipy.misc import imread
+from preprocess.normalize import preprocess_signature
+import signet
+from cnn_model import CNNModel
+import numpy as np
+import sys
+import os
+import scipy.io
+
 
 UPLOAD_FOLDER = 'tmp'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
+THRESHOLD = 10
 
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 # limit maximum allowed payload to 16 megabytes
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+
+def compare_signatures(path1,path2):
+
+    canvas_size = (952, 1360)
+    max1 = 0
+    max2 = 0
+
+    # Load the model
+    model_weight_path = 'models/signet.pkl'
+    model = CNNModel(signet, model_weight_path)
+
+    original1 = imread(path1, flatten=1)
+    processed1 = preprocess_signature(original1, canvas_size)
+
+    original2 = imread(path2, flatten=1)
+    processed2 = preprocess_signature(original2, canvas_size)
+
+    feature_vector1 = model.get_feature_vector(processed1)
+    feature_vector2 = model.get_feature_vector(processed2) 
+    feature_vector1 = feature_vector1.T
+    feature_vector2 = feature_vector2.T 
+
+    dist = (abs(feature_vector1**2 - feature_vector2**2))**(0.5)
+    print(dist)
+
+    for idx, val in enumerate(dist):
+        # print(val.shape)
+        if np.isnan(val):
+            dist[idx] = 0
+
+    dist = np.sum(dist)
+    print(dist)
+
+    return dist
 
 
 @app.route("/")
@@ -53,13 +99,18 @@ def verify():
         
         if file and allowed_file(file.filename) and gt_file and allowed_file(gt_file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            fp1 = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(fp1)
 
             gt_filename = secure_filename(gt_file.filename)
-            gt_file.save(os.path.join(app.config['UPLOAD_FOLDER'], gt_filename))
+            fp2 = os.path.join(app.config['UPLOAD_FOLDER'], gt_filename)
+            gt_file.save(fp2)
 
-            return filename
+            result = compare_signatures(fp1, fp2)
+
+            return render_template("result.html", dist=result)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
