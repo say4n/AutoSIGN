961ab3eff03445c99b46c19e13d0657af0aea0d2
Ayan Sinha Mahapatra <ayansmahapatra@gmail.com>
2018-06-27 11:54:51 +0530

    Threshold Function Commit

--- comp_sign.py
+++ comp_sign.py
@@ -1,21 +1,20 @@
+Thresholds
 
-#Thresholds
+main_thr_1 = 950.0 # Normal
+main_thr_2 = 800.0 # High
+main_thr_3 = 700.0 # Very High
 
-main_thr_1 = 0.0 # Normal
-main_thr_2 = 0.0 # High
-main_thr_3 = 0.0 # Very High
+same_upper  = 1050.0
+same_middle = 800.0
+same_lower  = 600.0
 
-same_upper  = 0.0
-same_middle = 0.0
-same_lower  = 0.0
+forg_upper  = 1350.0
+forg_middle = 1120.0
+forg_lower  = 810.0
 
-forg_upper  = 0.0
-forg_middle = 0.0
-forg_lower  = 0.0
-
-diff_upper  = 0.0
-diff_middle = 0.0
-diff_lower  = 0.0
+diff_upper  = 1800.0
+diff_middle = 1345.0
+diff_lower  = 1150.0
 
 # level 0 = Normal, 1 = High, 2 = Very High
 def compare_signatures(path1,path2,level):
@@ -53,14 +52,14 @@ def compare_signatures(path1,path2,level):
     dist = np.sum(dist)
 
     main_thr = 0.0
+    decision = -1
+
     if level is 0:
         main_thr = main_thr_1
     elif level is 1:
         main_thr = main_thr_2
-    elif level is 3:
+    elif level is 2:
         main_thr = main_thr_3
-
-    decision = -1
 
     if(dist<main_thr):
         decision = 1
@@ -71,9 +70,48 @@ def compare_signatures(path1,path2,level):
     forg_per = 0.0
     diff_per = 0.0
 
+    # Calculating same_per
+    if(dist<same_lower):
+        same_per = 100 - ((dist-0)/(same_lower-0))*5.0
+    elif(dist<same_middle):
+        same_per = 95 - ((dist-same_lower)/(same_middle-same_lower))*45
+    elif(dist<same_upper):
+        same_per = 50 - ((dist-same_middle)/(same_upper-same_middle))*45
+    elif(dist>1350):
+        same_per = 0
+    elif(dist>same_upper):
+        same_per = 5 - ((dist-same_upper)/(1350-same_upper))*5
 
+    # Calculating forg_per
+    if((dist<forg_lower)&(dist>=700)):
+        forg_per = ((dist-700)/(forg_lower-700))*15
+    elif(dist<700):
+        forg_per = 0.0
+    elif(dist<forg_middle):
+        forg_per = 15 + ((dist-forg_lower)/(forg_middle-forg_lower))*60
+    elif(dist<forg_upper):
+        forg_per = 15 + ((dist-forg_middle)/(forg_upper-forg_middle))*60
+    elif(dist>=2000):
+        forg_per = 0.0
+    elif(dist>forg_upper):
+        forg_per = ((dist-forg_upper)/(2000-forg_upper))*15
 
+    # Calculating diff_per
+    if(dist<=1000):
+        diff_per = 0.0
+    elif(dist<diff_lower):
+        diff_per = ((dist-1000)/(diff_lower-1000))*5.0
+    elif(dist<diff_middle):
+        diff_per = 5 + ((dist-diff_lower)/(diff_middle-diff_lower))*45
+    elif(dist<diff_upper):
+        diff_per = 50 + ((dist-diff_middle)/(diff_upper-diff_middle))*45
+    elif(dist>diff_upper):
+        diff_per = 95 + ((dist-same_upper)/(3000-same_upper))*5
 
-    
+    if(dist>=3000):
+        same_per = 0.0
+        forg_per = 0.0
+        diff_per = 100.0
+
 
     return dist,decision,same_per,forg_per,diff_per
