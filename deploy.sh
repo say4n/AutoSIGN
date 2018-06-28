8dd0d9f4f322c282971cffa67192847400505530
Sayan Goswami <goswami.sayan47@gmail.com>
2018-06-28 17:40:59 +0530

    public server
    http://flask.pocoo.org/docs/0.12/quickstart/ - Externally Visible Server

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 mkdir .log 2> /dev/null
-DEBUG=0 authbind gunicorn -w 16 -b 127.0.0.1:80 main:app --access-logfile .log/access.log --error-logfile .log/general.log
+DEBUG=0 authbind gunicorn -w 16 -b 0.0.0.0:80 main:app --access-logfile .log/access.log --error-logfile .log/general.log
